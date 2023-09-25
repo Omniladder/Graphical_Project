49045c78e85078872104bfe8f10a5ce461329f8c
Dustin O'Brien <Omniladder>
2023-09-25 11:50:11 -0400

    Simplified running code and added error detection Almost ready for displaying shapes

--- Open_GL_Basics.h
+++ Open_GL_Basics.h
@@ -7,8 +7,104 @@
 #include <GL/glew.h>
 #include "glfw3.h" //Graphical usage
 
-void PreCode(GLFWwindow* window)
+void APIENTRY GLDebugMessageCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar *msg, const void *data)
 {
+    const char* _source;
+    const char* _type;
+    const char* _severity;
+
+    switch (source) {
+        case GL_DEBUG_SOURCE_API:
+        _source = "API";
+        break;
+
+        case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
+        _source = "WINDOW SYSTEM";
+        break;
+
+        case GL_DEBUG_SOURCE_SHADER_COMPILER:
+        _source = "SHADER COMPILER";
+        break;
+
+        case GL_DEBUG_SOURCE_THIRD_PARTY:
+        _source = "THIRD PARTY";
+        break;
+
+        case GL_DEBUG_SOURCE_APPLICATION:
+        _source = "APPLICATION";
+        break;
+
+        case GL_DEBUG_SOURCE_OTHER:
+        _source = "UNKNOWN";
+        break;
+
+        default:
+        _source = "UNKNOWN";
+        break;
+    }
+
+    switch (type) {
+        case GL_DEBUG_TYPE_ERROR:
+        _type = "ERROR";
+        break;
+
+        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
+        _type = "DEPRECATED BEHAVIOR";
+        break;
+
+        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
+        _type = "UDEFINED BEHAVIOR";
+        break;
+
+        case GL_DEBUG_TYPE_PORTABILITY:
+        _type = "PORTABILITY";
+        break;
+
+        case GL_DEBUG_TYPE_PERFORMANCE:
+        _type = "PERFORMANCE";
+        break;
+
+        case GL_DEBUG_TYPE_OTHER:
+        _type = "OTHER";
+        break;
+
+        case GL_DEBUG_TYPE_MARKER:
+        _type = "MARKER";
+        break;
+
+        default:
+        _type = "UNKNOWN";
+        break;
+    }
+
+    switch (severity) {
+        case GL_DEBUG_SEVERITY_HIGH:
+        _severity = "HIGH";
+        break;
+
+        case GL_DEBUG_SEVERITY_MEDIUM:
+        _severity = "MEDIUM";
+        break;
+
+        case GL_DEBUG_SEVERITY_LOW:
+        _severity = "LOW";
+        break;
+
+        case GL_DEBUG_SEVERITY_NOTIFICATION:
+        _severity = "NOTIFICATION";
+        break;
+
+        default:
+        _severity = "UNKNOWN";
+        break;
+    }
+
+    printf(" OPENGL NOTIFICATION :: %d: %s of %s severity, raised from %s: %s\n", id, _type, _severity, _source, msg);
+}
+
+void PreCode(GLFWwindow* window, GLuint shader)
+{
+	glUseProgram(shader);
     // Clear the screen
         glClear(GL_COLOR_BUFFER_BIT);
 
@@ -58,6 +154,8 @@ GLFWwindow* Open_window(int windows_width, int windows_height, const char* windo
     glfwMakeContextCurrent(window);
     glewInit();
 
+    glEnable(GL_DEBUG_OUTPUT);
+	glDebugMessageCallback(GLDebugMessageCallback, nullptr);
 
     return window;
 }
@@ -141,7 +239,6 @@ void Runtime_Reloading(GLFWwindow* window, unsigned int &shader, int key, const 
         shader = activateShaders(vertexShader, fragmentShader, vID, fID);
     }
 }
-
 
 
 #endif
